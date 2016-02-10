3e89153e008337bef5505cbdac3da5ec25fb58ae
Michael Miller <icy.arctic.fox@gmail.com>
2016-02-10 11:41:59 -0700

    Add top-level include script
    Including this script will provide RGSS (with RPG classes) to Ruby scripts.

---
+++ lib/rpg_maker_rgss3.rb
@@ -0,0 +1,6 @@
+# Include RGSS functionality.
+require_relative 'functions'
+require_relative 'rgss' # This is a compiled native extension.
+
+# Include classes and modules provided by RPG Maker.
+require_relative 'rpg'
