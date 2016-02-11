f8fbe7118df75acb6addf5748cf491b24f6cdc63
Michael Miller <icy.arctic.fox@gmail.com>
2016-02-10 22:57:05 -0700

    Add extern C to fix linking errors

--- include/binding/binding.h
+++ include/binding/binding.h
@@ -1,6 +1,16 @@
 #ifndef RGSS_BINDING_H
 #define RGSS_BINDING_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <ruby.h>
+
 void Init_rgss ();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //RGSS_BINDING_H
