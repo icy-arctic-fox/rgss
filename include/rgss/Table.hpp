f25950481bddccf61549c940af6929250e2e6bff
Michael Miller <icy.arctic.fox@gmail.com>
2016-02-13 04:50:44 -0700

    Made Table "dimensionally aware"
    The Table class now tracks how many dimensions it was created it.
    Additional constructors and methods have been added.
    The method used determines how many dimensions the table has.

--- include/rgss/Table.hpp
+++ include/rgss/Table.hpp
@@ -8,20 +8,27 @@ namespace RGSS
     class Table
     {
     private:
-        int _w, _h, _d;
+        int _dimensions, _w, _h, _d;
         sf::Int16 *_data; // Flat array arranged as x, y, z.
 
-        int _xyzToIndex (int x, int y, int z) const;
         static int _whdToSize (int w, int h, int d);
+        int _xyzToIndex (int x, int y, int z) const;
+        void _resize (int w, int h, int d);
 
     public:
         Table ();
+        Table (int w);
+        Table (int w, int h);
         Table (int w, int h, int d);
+        ~Table ();
         int getWidth () const;
         int getHeight () const;
         int getDepth () const;
-        sf::Int16 get (int x, int y, int z) const;
-        void set (int x, int y, int z, sf::Int16 value);
+        int getDimensions () const;
+        sf::Int16 get (int x, int y = 0, int z = 0) const;
+        void set (sf::Int16 value, int x, int y = 0, int z = 0);
+        void resize (int w);
+        void resize (int w, int h);
         void resize (int w, int h, int d);
         Table *clone () const;
     };
--- src/rgss/Table.cpp
+++ src/rgss/Table.cpp
@@ -3,12 +3,6 @@
 
 namespace RGSS
 {
-    int Table::_xyzToIndex (int x, int y, int z) const
-    {
-        // TODO: Throw exception for invalid indices.
-        return x + (y * _w + (z * _w * _h));
-    }
-
     int Table::_whdToSize (int w, int h, int d)
     {
         // TODO: Throw exception for invalid dimensions.
@@ -16,21 +10,86 @@ namespace RGSS
         return size < 0 ? 1 : size;
     }
 
+    int Table::_xyzToIndex (int x, int y, int z) const
+    {
+        // TODO: Throw exception for invalid indices.
+        return x + (y * _w + (z * _w * _h));
+    }
+
+    void Table::_resize (int w, int h, int d)
+    {
+        int newSize = _whdToSize(w, h, d);
+
+        if(newSize > 0)
+        {
+            int minW = w < _w ? w : _w;
+            int minH = h < _h ? h : _h;
+            int minD = d < _d ? d : _d;
+            sf::Int16 *newData = new sf::Int16[newSize];
+
+            sf::Int16 *source = _data, *dest = newData;
+            for(int z = 0; z < minD; ++z)
+                for(int y = 0; y < minH; ++y, source += _w, dest += w)
+                    memcpy(source, dest, minW);
+
+            if(_data)
+                delete _data;
+            _data = newData;
+        }
+        else if(_data)
+            delete _data;
+
+        _w = w;
+        _h = h;
+        _d = d;
+    }
+
     Table::Table ()
     {
+        _dimensions = 0;
         _w = 0;
         _h = 0;
         _d = 0;
         _data = NULL;
     }
 
+    Table::Table (int w)
+    {
+        _dimensions = 1;
+        _w = w;
+        _h = 1;
+        _d = 1;
+
+        int size = _whdToSize(_w, _h, _d);
+        _data = new sf::Int16[size];
+    }
+
+    Table::Table (int w, int h)
+    {
+        _dimensions = 2;
+        _w = w;
+        _h = h;
+        _d = 1;
+
+        int size = _whdToSize(_w, _h, _d);
+        _data = new sf::Int16[size];
+    }
+
     Table::Table (int w, int h, int d)
     {
-        int size = _whdToSize(w, h, d);
+        _dimensions = 3;
         _w = w;
         _h = h;
         _d = d;
+
+        int size = _whdToSize(_w, _h, _d);
         _data = new sf::Int16[size];
+    }
+
+    Table::~Table ()
+    {
+        if(_data)
+            delete _data;
     }
 
     int Table::getWidth () const
@@ -48,45 +107,59 @@ namespace RGSS
         return _d;
     }
 
+    int Table::getDimensions () const
+    {
+        return _dimensions;
+    }
+
     sf::Int16 Table::get (int x, int y, int z) const
     {
         int index = _xyzToIndex(x, y, z);
         return _data[index];
     }
 
-    void Table::set (int x, int y, int z, sf::Int16 value)
+    void Table::set (sf::Int16 value, int x, int y, int z)
     {
         int index = _xyzToIndex(x, y, z);
         _data[index] = value;
     }
 
+    void Table::resize (int w)
+    {
+        _dimensions = 1;
+        if(w < 0)
+            w = 0;
+        _resize(w, 1, 1);
+    }
+
+    void Table::resize (int w, int h)
+    {
+        _dimensions = 2;
+        if(w < 0)
+            w = 0;
+        if(h < 0)
+            h = 0;
+        _resize(w, h, 1);
+    }
+
     void Table::resize (int w, int h, int d)
     {
-        int newSize = _whdToSize(w, h, d);
-        int minW = w < _w ? w : _w;
-        int minH = h < _h ? h : _h;
-        int minD = d < _d ? d : _d;
-        sf::Int16 *newData = new sf::Int16[newSize];
-
-        sf::Int16 *source = _data, *dest = newData;
-        for(int z = 0; z < minD; ++z)
-            for(int y = 0; y < minH; ++y, source += _w, dest += w)
-                memcpy(source, dest, minW);
-
-        _w = w;
-        _h = h;
-        _d = d;
-
-        if(_data)
-            delete _data;
-        _data = newData;
+        _dimensions = 3;
+        if(w < 0)
+            w = 0;
+        if(h < 0)
+            h = 0;
+        if(d < 0)
+            d = 0;
+        _resize(w, h, d);
     }
 
     Table *Table::clone () const
     {
         int size = _whdToSize(_w, _h, _d);
         Table *copy = new Table(_w, _h, _d);
-        memcpy(copy->_data, _data, size * sizeof(sf::Int16));
+        if(_data)
+            memcpy(copy->_data, _data, size * sizeof(sf::Int16));
         return copy;
     }
 }
