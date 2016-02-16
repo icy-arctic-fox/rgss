dfeea89b736ec760cc8042cdb7a5d62b302ab477
Michael Miller <icy.arctic.fox@gmail.com>
2016-02-16 13:45:22 -0700

    Add missing functions
    rgss_main and rgss_stop are handled by the native extension.
    The other top-level functions RGSS provides are handled in Ruby.

--- src/binding/functions.cpp
+++ src/binding/functions.cpp
@@ -6,7 +6,21 @@ VALUE function_msgbox (int argc, VALUE *argv)
     return Qnil;
 }
 
+VALUE function_rgss_main ()
+{
+    // TODO
+    return Qnil;
+}
+
+VALUE function_rgss_stop ()
+{
+    // TODO
+    return Qnil;
+}
+
 void initFunctions ()
 {
-    rb_define_global_function("msgbox", RB_FUNC(function_msgbox), -1);
+    rb_define_global_function("rgss_main", RB_FUNC(function_rgss_main),  0);
+    rb_define_global_function("rgss_stop", RB_FUNC(function_rgss_stop),  0);
+    rb_define_global_function("msgbox",    RB_FUNC(function_msgbox),    -1);
 }
