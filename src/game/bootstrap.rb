4331323e0d881cca42cc6fa22b7ac57b29724d49
Michael Miller <icy.arctic.fox@gmail.com>
2016-02-20 10:04:27 -0700

    Abort as soon as a script has an error
    Don't continue evaluating scripts if an error has occurred.

--- src/game/bootstrap.rb
+++ src/game/bootstrap.rb
@@ -8,14 +8,14 @@ scripts = File.open(scripts_file, 'rb') do |f|
   Marshal.load(f)
 end
 
-scripts.each do |script|
-  contents = Zlib.inflate(script[2])
-  begin
+begin
+  scripts.each do |script|
+    contents = Zlib.inflate(script[2])
     eval contents, nil, script[1]
-  rescue Object => ex
-    $stderr.puts "#{ex.class}: #{ex.message}"
-    ex.backtrace.each do |item|
-      $stderr.puts "\tfrom #{item}"
-    end
+  end
+rescue Object => ex
+  $stderr.puts "#{ex.class}: #{ex.message}"
+  ex.backtrace.each do |item|
+    $stderr.puts "\tfrom #{item}"
   end
 end
